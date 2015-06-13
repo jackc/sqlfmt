2e98df7fca45451f37a08df2d2f0e13935cc5e89
Jack Christensen <jack@jackchristensen.com>
2015-06-13 15:14:09 -0500

    WIP interfaces

--- main.go
+++ main.go
@@ -10,3 +10,17 @@ func main() {
 	fmt.Println("Hello, world")
 	io.Copy(os.Stdout, os.Stdin)
 }
+
+type SelectStmt struct {
+  Fields    []string
+  FromTable string
+}
+
+type PrettyPrinter interface {
+  io.Writer
+  Indent(string)
+  Unindent()
+  Newline() error
+}
+
+func (s *SelectStmt)
